0318fd28af66d4a366f0d0245e60a8b300eeca65
Anastasia Martova <nesti_lash@mail.ru>
2025-01-30 15:10:21 +0300

    added receiving of all currencies

--- src/main/java/currency/exchange/mapper/CurrencyMapper.java
+++ src/main/java/currency/exchange/mapper/CurrencyMapper.java
@@ -1,8 +1,13 @@
 package currency.exchange.mapper;
 
+import currency.exchange.dto.CurrencyItemDto;
+import currency.exchange.dto.CurrencyList;
 import org.mapstruct.Mapper;
 import currency.exchange.dto.CurrencyDto;
 import currency.exchange.entity.Currency;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface CurrencyMapper {
@@ -10,5 +15,19 @@ public interface CurrencyMapper {
     CurrencyDto convertToDto(Currency currency);
 
     Currency convertToEntity(CurrencyDto currencyDto);
+
+    CurrencyItemDto convertToItemDto(Currency currency);
+
+    default CurrencyList convertToDtoList(List<Currency> currencies) {
+        if (currencies == null || currencies.isEmpty()) {
+            return new CurrencyList();
+        }
+
+        List<CurrencyItemDto> currencyItemDtos = currencies.stream()
+                .map(this::convertToItemDto)
+                .collect(Collectors.toList());
+
+        return new CurrencyList(currencyItemDtos);
+    }
 
 }
